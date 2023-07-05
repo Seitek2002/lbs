98a07ff034e0c134f7a4a51a49c35a978fdfc180
Seitek2002 <seitek_2002@bk.ru>
2023-07-06 02:55:10 +0600

    some changes

--- src/components/DrawMap/index.jsx
+++ src/components/DrawMap/index.jsx
@@ -34,67 +34,76 @@ const DrawMap = () => {
       const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
       setBounds(newBounds)
     }
-    console.log(route);
   }, [route])
 
   React.useEffect(() => {
-    dispatch(
-      routerActions.fetchRoute({
-        original: [42.86476777774923, 74.55397209694085],
-        destination: [42.8623880844005, 74.55248618718157],
-      })
-    )
+    // dispatch(
+    //   routerActions.fetchRoute({
+    //     original: [42.86239377156107, 74.5525118880723],
+    //     destination: [42.8647615727689, 74.55400601763478],
+    //   })
+    // )
+
+    const ws = new WebSocket('ws://192.168.243.202:8000/ws/socket-server/chat/1')
+
+    ws.onmessage = async e => {
+      const data = await JSON.parse(e.data)
+      // console.log(data.result?.routes[0]?.geometry?.coordinates);
+      // handleSetBounds(data.result?.routes[0]?.geometry?.coordinates)
+      // const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
+      // console.log(newBounds);
+      // setBounds(newBounds)
+
+      const points = data.result.routes[0].geometry.coordinates.map(arr => [arr[1], arr[0]])
+      setPoints(points)
+      const originPoint = { lat: points[0][0], lng: points[0][1] }
+      const destinationPoint = {
+        lat: points[points.length - 1][0],
+        lng: points[points.length - 1][1],
+      }
+      setOriginMarker(originPoint)
+      setDestinationMarker(destinationPoint)
+      const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
+      console.log(newBounds)
+      setBounds(newBounds)
+    }
+
+    const handleCheck = (x, y) => {
+      console.log(x, y)
+      ws.send(
+        JSON.stringify({
+          a: `${y},${x}`,
+          b: '74.55400601763478,42.8647615727689',
+        })
+      )
+      // dispatch(
+      //   routerActions.fetchRoute({
+      //     original: [42.86476777774923, 74.55397209694085],
+      //     destination: [x, y],
+      //   })
+      // )
+    }
+
+    if (navigator.geolocation) {
+      const watchId = navigator.geolocation.watchPosition(
+        position => {
+          const latitude = position.coords.latitude
+          const longitude = position.coords.longitude
+
+          handleCheck(latitude, longitude)
+        },
+        function (error) {
+          console.log(error)
+        }
+      )
+    } else {
+      console.log('Геолокация не поддерживается вашим браузером')
+    }
   }, [])
 
   const handleSetBounds = bounds => {
     setBounds(bounds)
   }
-
-  // const ws = new WebSocket('ws://176.126.164.218:8000/ws/socket-server/chat/1')
-
-  // ws.onmessage = async (e) => {
-  //   const data = await JSON.parse(e.data)
-  // }
-
-  const handleCheck = (x,y) => {
-    // ws.send(
-    //   JSON.stringify({
-    //     x,
-    //     y
-    //   })
-    // )
-    dispatch(
-      routerActions.fetchRoute({
-        original: [42.86476777774923, 74.55397209694085],
-        destination: [x, y],
-      })
-    )
-  }
-
-  if (navigator.geolocation) {
-    const options = {
-      enableHighAccuracy: true, // Запрос на более высокую точность
-      timeout: 5000, // Максимальное время ожидания
-      maximumAge: 0 // Не использовать кэшированное местоположение
-    };
-
-    const watchId = navigator.geolocation.watchPosition(
-      function (position) {
-        // Обработка обновленного местоположения
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        handleCheck(latitude, longitude)
-      },
-      function (error) {
-        console.log(error);
-      },
-      options
-    );
-  } else {
-    console.log('Геолокация не поддерживается вашим браузером');
-  }
-
 
   return (
     <MapContainer bounds={bounds} center={[42.844827, 74.588]} zoom={12}>
--- src/features/router/routerSaga.js
+++ src/features/router/routerSaga.js
@@ -5,6 +5,7 @@ import { routerActions } from './routerSlice';
 function* fetchRoute(action) {
   try {
     const response = yield call(routerApi.getRoute, action.payload);
+    console.log(response);
     yield put(routerActions.fetchRouteSuccess(response));
   } catch (error) {
     yield put(routerActions.fetchRouteFailed(error));
