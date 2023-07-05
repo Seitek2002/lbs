8276aca4c8c979c8ee04a5dda31e39b82cc3e4a2
Seitek2002 <seitek_2002@bk.ru>
2023-07-06 03:32:47 +0600

    some changes

--- src/components/DrawMap/index.jsx
+++ src/components/DrawMap/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MapContainer, TileLayer, Popup, Polyline, useMapEvent } from 'react-leaflet'
 import { useDispatch, useSelector } from 'react-redux'
 import AutoFitBound from '../../components/AutoFitBound'
@@ -20,86 +20,62 @@ const DrawMap = () => {
     return null
   }
 
-  React.useEffect(() => {
-    if (route) {
-      const points = route.routes[0].geometry.coordinates.map(arr => [arr[1], arr[0]])
-      setPoints(points)
-      const originPoint = { lat: points[0][0], lng: points[0][1] }
-      const destinationPoint = {
-        lat: points[points.length - 1][0],
-        lng: points[points.length - 1][1],
-      }
-      setOriginMarker(originPoint)
-      setDestinationMarker(destinationPoint)
-      const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
-      setBounds(newBounds)
-    }
-  }, [route])
+  // React.useEffect(() => {
+  //   if (route) {
+  //     const points = route.routes[0].geometry.coordinates.map(arr => [arr[1], arr[0]])
+  //     setPoints(points)
+  //     const originPoint = { lat: points[0][0], lng: points[0][1] }
+  //     const destinationPoint = {
+  //       lat: points[points.length - 1][0],
+  //       lng: points[points.length - 1][1],
+  //     }
+  //     setOriginMarker(originPoint)
+  //     setDestinationMarker(destinationPoint)
+  //     const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
+  //     setBounds(newBounds)
+  //   }
+  // }, [route])
 
-  React.useEffect(() => {
-    // dispatch(
-    //   routerActions.fetchRoute({
-    //     original: [42.86239377156107, 74.5525118880723],
-    //     destination: [42.8647615727689, 74.55400601763478],
-    //   })
-    // )
+  const ws = new WebSocket('ws://192.168.243.202:8000/ws/socket-server/chat/1')
 
-    const ws = new WebSocket('ws://192.168.243.202:8000/ws/socket-server/chat/1')
+  const handleCheck = (x, y) => {
+    ws.send(
+      JSON.stringify({
+        a: `${y},${x}`,
+        b: '74.55400601763478,42.8647615727689',
+      })
+    )
+  }
 
-    ws.onmessage = async e => {
-      const data = await JSON.parse(e.data)
-      // console.log(data.result?.routes[0]?.geometry?.coordinates);
-      // handleSetBounds(data.result?.routes[0]?.geometry?.coordinates)
-      // const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
-      // console.log(newBounds);
-      // setBounds(newBounds)
-
-      const points = data.result.routes[0].geometry.coordinates.map(arr => [arr[1], arr[0]])
-      setPoints(points)
-      const originPoint = { lat: points[0][0], lng: points[0][1] }
-      const destinationPoint = {
-        lat: points[points.length - 1][0],
-        lng: points[points.length - 1][1],
-      }
-      setOriginMarker(originPoint)
-      setDestinationMarker(destinationPoint)
-      const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
-      console.log(newBounds)
-      setBounds(newBounds)
-    }
-
-    const handleCheck = (x, y) => {
-      console.log(x, y)
-      ws.send(
-        JSON.stringify({
-          a: `${y},${x}`,
-          b: '74.55400601763478,42.8647615727689',
-        })
-      )
-      // dispatch(
-      //   routerActions.fetchRoute({
-      //     original: [42.86476777774923, 74.55397209694085],
-      //     destination: [x, y],
-      //   })
-      // )
-    }
-
-    if (navigator.geolocation) {
-      const watchId = navigator.geolocation.watchPosition(
-        position => {
-          const latitude = position.coords.latitude
-          const longitude = position.coords.longitude
-
-          handleCheck(latitude, longitude)
-        },
-        function (error) {
-          console.log(error)
-        }
-      )
-    } else {
-      console.log('Геолокация не поддерживается вашим браузером')
-    }
+  useEffect(() => {
+    navigator.geolocation.watchPosition(position => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+      handleCheck(latitude, longitude)
+    })
   }, [])
+
+  ws.onmessage = async e => {
+    const data = await JSON.parse(e.data)
+
+    const points = data.result.routes[0].geometry.coordinates.map(arr => [arr[1], arr[0]])
+    setPoints(points)
+    const originPoint = { lat: points[0][0], lng: points[0][1] }
+    const destinationPoint = {
+      lat: points[points.length - 1][0],
+      lng: points[points.length - 1][1],
+    }
+    setOriginMarker(originPoint)
+    setDestinationMarker(destinationPoint)
+    const newBounds = [originPoint, destinationPoint].map(m => [m.lat, m.lng])
+    setBounds(newBounds)
+
+    navigator.geolocation.watchPosition(position => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+      handleCheck(latitude, longitude)
+    })
+  }
 
   const handleSetBounds = bounds => {
     setBounds(bounds)
